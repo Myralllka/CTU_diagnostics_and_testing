ed8ce1dd69360fbd170c78e43b76b3e3ca3b0830
Mykola Morgunenko <Olli25010@gmail.com>
2022-12-04 16:29:54 +0100

    add report

--- semestral_project/Tag.py
+++
@@ -1,7 +0,0 @@
-class Node:
-    def __init__(self, number: int):
-        # Assume only 9 anchors!
-        self.number = number
-
-    def __str__(self):
-        return "({})".format(self.number)
